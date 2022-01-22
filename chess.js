ab69e43e373a59cb0cf0025b5b2e8b3274d6b7aa
CinematicGenius007 <ayush3642.be20@chitkara.edu.in>
2022-01-22 22:56:21 +0530

    init code

--- chess.js
+++ chess.js
@@ -1,66 +1,77 @@
 // Matrix - 8 x 8
-let chess_pieces = document.querySelectorAll('.chess-piece');
+let chessPieces = document.querySelectorAll('.chess-piece');
 
 for (let i = 0; i < 64; i++) {
-    chess_pieces[i].addEventListener("click", () => {
-        move(chess_pieces[i], i);
+    chessPieces[i].addEventListener("click", () => {
+        move(chessPieces[i], i);
     });
 }
 
+const pieces = ['whiteKing', 'whiteQueen', 'whiteRook', 'whiteBishop', 'whiteKnight', 'whitePawn',
+    'blackKing', 'blackQueen', 'blackRook', 'blackBishop', 'blackKnight', 'blackPawn'];
 
-// let chess_board;
+// const pieces = {
+//     whiteKing: '♔',
+//     whiteQueen: '♕',
+//     whiteRook: '♖',
+//     whiteBishop: '♗',
+//     whiteKnight: '♘',
+//     whitePawn: '♙',
+//     blackKing: '♔',
+//     blackQueen: '♕',
+//     blackRook: '♜',
+//     blackBishop: '♝',
+//     blackKnight: '♞',
+//     blackPawn: '♟'
+// }
 
 const TYPE = {
     WHITE: 'white',
     BLACK: 'black'
 };
 
-const pieces = new Map();
-pieces.set('♔', 'wk');
-pieces.set('♕', 'wq');
-pieces.set('♙', 'wp');
-pieces.set('♗', 'wb'); // thedi chaal
-pieces.set('♘', 'wn'); // dhai kadam
-pieces.set('♖', 'wr'); // bahaut sidha
-pieces.set('♚', 'bk');
-pieces.set('♛', 'bq');
-pieces.set('♟', 'bp');
-pieces.set('♝', 'bb'); // thedi chaal
-pieces.set('♞', 'bn'); // dhai kadam
-pieces.set('♜', 'br'); // bahaut sidha
-pieces.set('', 'empty');
-
-
 (function init_board() {
-    chess_pieces[0].innerHTML = chess_pieces[7].innerHTML = '♜';
-    chess_pieces[1].innerHTML = chess_pieces[6].innerHTML = '♞';
-    chess_pieces[2].innerHTML = chess_pieces[5].innerHTML = '♝';
-    chess_pieces[3].innerHTML = '♛';
-    chess_pieces[4].innerHTML = '♚';
-
-    for (let i = 8; i < 16; i++) {
-        chess_pieces[i].innerHTML = '♟'
-    }
-
-    chess_pieces[56].innerHTML = chess_pieces[63].innerHTML = '♖';
-    chess_pieces[57].innerHTML = chess_pieces[62].innerHTML = '♘';
-    chess_pieces[58].innerHTML = chess_pieces[61].innerHTML = '♗';
-    chess_pieces[59].innerHTML = '♕';
-    chess_pieces[60].innerHTML = '♔';
+    chessPieces[56].setAttribute('id', 'blackRook');
+    chessPieces[63].setAttribute('id', 'blackRook');
+    chessPieces[57].setAttribute('id', 'blackKnight');
+    chessPieces[62].setAttribute('id', 'blackKnight');
+    chessPieces[58].setAttribute('id', 'blackBishop');
+    chessPieces[61].setAttribute('id', 'blackBishop');
+    chessPieces[59].setAttribute('id', 'blackQueen');
+    chessPieces[60].setAttribute('id', 'blackKing');
 
     for (let i = 48; i < 56; i++) {
-        chess_pieces[i].innerHTML = '♙'
+        chessPieces[i].setAttribute('id', 'blackPawn');
+    }
+
+    chessPieces[0].setAttribute('id', 'whiteRook');
+    chessPieces[7].setAttribute('id', 'whiteRook');
+    chessPieces[1].setAttribute('id', 'whiteKnight');
+    chessPieces[6].setAttribute('id', 'whiteKnight');
+    chessPieces[5].setAttribute('id', 'whiteBishop');
+    chessPieces[2].setAttribute('id', 'whiteBishop');
+    chessPieces[3].setAttribute('id', 'whiteQueen');
+    chessPieces[4].setAttribute('id', 'whiteKing');
+
+    for (let i = 8; i < 16; i++) {
+        chessPieces[i].setAttribute('id', 'whitePawn');
     }
 
 })();
 
 let selected_index = -1;
 let possibleMoves = [];
+let possibleHits = [];
 let turn = TYPE.WHITE;
 let castle = {
     black: 0,
     white: 0
 };
+
+function checkTurn(el) {
+    return !!(el.id.match('^white.*') && turn === TYPE.WHITE || el.id.match('^black.*') && turn === TYPE.BLACK);
+
+}
 
 // Possible scenarios
 // 1. When no other pieces is selected.
@@ -70,173 +81,232 @@ let castle = {
 //    c. they try to select any other thing, their own piece or a blank piece.
 
 function move(el, i) {
-    let piece_val = pieces.get(el.innerHTML);
-    if (piece_val === 'empty') {
-        // third case is possible move
-
-        console.log([i, possibleMoves])
-        if (possibleMoves.includes(i)) {
-            turn = turn === TYPE.WHITE ? TYPE.BLACK : TYPE.WHITE;
-            let new_piece = chess_pieces[selected_index].innerHTML;
-            chess_pieces[selected_index].innerHTML = null;
-            el.innerHTML = new_piece;
-            chess_pieces[selected_index].classList.remove('chess-piece-selected');
-
-        } else {
-            if (selected_index !== null && i !== selected_index) {
-                chess_pieces[selected_index].classList.remove('chess-piece-selected');
-            }
-        }
-
-        for (let j = 0; j < possibleMoves.length; j++) {
-            if (j !== -1) {
-                chess_pieces[possibleMoves[j]].classList.remove('chess-piece-possible-moves');
-                chess_pieces[possibleMoves[j]].classList.remove('chess-piece-in-danger');
-            }
-        }
-
-        possibleMoves = []
-
-    }
-    else {
-        if (piece_val.match('^w.$') && turn === TYPE.WHITE || piece_val.match('^b.$') && turn === TYPE.BLACK) {
-            if (selected_index !== -1 && selected_index !== i) {
-                chess_pieces[selected_index].classList.remove('chess-piece-selected');
-            }
-            for (let j = 0; j < possibleMoves.length; j++) {
-                if (j !== -1) {
-                    chess_pieces[possibleMoves[j]].classList.remove('chess-piece-possible-moves');
-                    chess_pieces[possibleMoves[j]].classList.remove('chess-piece-in-danger');
-                }
-            }
-            possibleMoves = []
+    console.log(el, i, possibleMoves, possibleHits, selected_index)
+    let pieceVal = el.id;
+    if (possibleMoves.includes(i) || possibleHits.includes(i)) {
+        turn = turn === TYPE.WHITE ? TYPE.BLACK : TYPE.WHITE;
+        let new_piece = chessPieces[selected_index].id;
+        chessPieces[selected_index].removeAttribute('id');
+        el.setAttribute('id', new_piece);
+        freePossibleHighlights()
+    } else {
+        if (i === selected_index) {
+            el.classList.remove('chess-piece-selected');
+            freePossibleHighlights();
+        } else if (pieceVal === '') {
+            freePossibleHighlights()
+        } else if (pieceVal.match('^white.*') && turn === TYPE.WHITE || pieceVal.match('^black.*') && turn === TYPE.BLACK) {
+            freePossibleHighlights();
             el.classList.add('chess-piece-selected');
             selected_index = i;
-            switch (piece_val) {
-                case 'wp':
-                    pawn(el, i, TYPE.WHITE);
+            switch (pieceVal) {
+                case 'whitePawn':
+                case 'blackPawn':
+                    pawn(i);
                     break;
-                case 'bp':
-                    pawn(el, i, TYPE.BLACK);
+                case 'whiteKing':
+                case 'blackKing':
+                    king(i);
                     break;
-                case 'wk':
-                case 'bk':
-                    king(el, i);
+                case 'whiteQueen':
+                case 'blackQueen':
+                    queen(i);
                     break;
-                case 'wq':
-                case 'bq':
-                    queen(el, i);
+                case 'whiteKnight':
+                case 'blackKnight':
+                    knight(i);
                     break;
-                case 'wn':
-                case 'bn':
-                    knight(el, i);
+                case 'whiteBishop':
+                case 'blackBishop':
+                    bishop(i);
                     break;
-                case 'wb':
-                case 'bb':
-                    bishop(el, i);
-                    break;
-                case 'wr':
-                case 'br':
-                    rook(el, i);
+                case 'whiteRook':
+                case 'blackRook':
+                    rook(i);
                     break;
             }
         }
     }
-
 }
 
 function highlight_possible_moves() {
-    // console.log(possibleMoves)
-    // console.log(selected_index)
-    for (let k = 0; k < (possibleMoves.includes(-1) ? possibleMoves.indexOf(-1) : possibleMoves.length); k++) {
-        chess_pieces[possibleMoves[k]].classList.add('chess-piece-possible-moves');
-    }
-    if (possibleMoves.includes(-1)) {
-        for (let l = possibleMoves.indexOf(-1) + 1; l < possibleMoves.length; l++) {
-            chess_pieces[possibleMoves[l]].classList.add('chess-piece-in-danger');
-        }
-    }
-
+    for (let k of possibleMoves)
+        chessPieces[k].classList.add('chess-piece-possible-moves');
+    for (let k of possibleHits)
+        chessPieces[k].classList.add('chess-piece-in-danger');
 }
 
-function pawn(el, i, type) {
-    if (type === TYPE.WHITE) {
+function freePossibleHighlights() {
+    if (selected_index !== -1) {
+        chessPieces[selected_index].classList.remove('chess-piece-selected');
+        selected_index = -1;
+    }
+    for (let cell of possibleMoves)
+        chessPieces[cell].classList.remove('chess-piece-possible-moves');
+    for (let cell of possibleHits)
+        chessPieces[cell].classList.remove('chess-piece-in-danger');
+    possibleMoves.splice(0, possibleMoves.length);
+    possibleHits.splice(0, possibleHits.length);
+}
+
+function pawn(i) {
+    if (turn === TYPE.BLACK) {
         // check for 2 moves or 1 move // [8 -> 16, 24 -> 17], [16, 24, -1, 17]
+        if (i < 8)
+            return;
         if (i >= 48 && i < 56) {
-            possibleMoves = [i - 8, i - 16];
-        }
-        else if (i > 7) {
-            possibleMoves = [i - 8];
-        }
+            possibleMoves.push(i - 8, i - 16);
+            if (pieces.includes(chessPieces[i - 16].id))
+                possibleMoves.pop();
+        } else if (i > 7)
+            possibleMoves.push(i - 8);
+        // If something is obstructing the pawn
+        if (pieces.includes(chessPieces[i - 8].id))
+            possibleMoves.splice(0, possibleMoves.length);
         // possible hits
         if (i % 8 === 0) {
-            if (pieces.get(chess_pieces[i - 7].innerHTML).match('^b.$')) {
-                possibleMoves.push([-1, i - 7]);
-            }
-        }
-        else if ((i + 1) % 8 === 0) {
-            if (pieces.get(chess_pieces[i - 9].innerHTML).match('^b.$')) {
-                possibleMoves.push([-1, i - 9]);
-            }
-        }
-        else {
-            if (pieces.get(chess_pieces[i - 7].innerHTML).match('^b.$')) {
-                possibleMoves.push([-1, i - 7]);
-            }
-            if (pieces.get(chess_pieces[i - 9].innerHTML).match('^b.$')) {
-                if (possibleMoves.includes(-1)) {
-                    possibleMoves.push([i - 9]);
-                }
-                else {
-                    possibleMoves.push([-1, i - 9]);
-                }
-            }
+            if (chessPieces[i - 7].id.match('^white.*'))
+                possibleHits.push(i - 7);
+        } else if ((i + 1) % 8 === 0) {
+            if (chessPieces[i - 9].id.match('^white.*'))
+                possibleHits.push(i - 9);
+        } else {
+            if (chessPieces[i - 7].id.match('^white.*'))
+                possibleHits.push(i - 7);
+            if (chessPieces[i - 9].id.match('^white.*'))
+                possibleHits.push(i - 9);
         }
 
     } else {
+        if (i >= 56)
+            return;
         if (i >= 8 && i < 16) {
-            possibleMoves = [i + 8, i + 16];
-        }
-        else if (i > 7) {
-            possibleMoves = [i + 8];
-        }
+            possibleMoves.push(i + 8, i + 16);
+            if (pieces.includes(chessPieces[i + 16].id))
+                possibleMoves.pop();
+        } else if (i < 56)
+            possibleMoves.push(i + 8);
+        // for pawn obstruction
+        if (pieces.includes(chessPieces[i + 8].id))
+            possibleMoves.splice(0, possibleMoves.length);
         // possible hits
         if (i % 8 === 0) {
-            if (pieces.get(chess_pieces[i + 7].innerHTML).match('^b.$')) {
-                possibleMoves.push([-1, i + 7]);
-            }
-        }
-        else if ((i + 1) % 8 === 0) {
-            if (pieces.get(chess_pieces[i + 9].innerHTML).match('^b.$')) {
-                possibleMoves.push([-1, i + 9]);
-            }
-        }
-        else {
-            if (pieces.get(chess_pieces[i + 7].innerHTML).match('^b.$')) {
-                possibleMoves.push([-1, i + 7]);
-            }
-            if (pieces.get(chess_pieces[i + 9].innerHTML).match('^b.$')) {
-                if (possibleMoves.includes(-1)) {
-                    possibleMoves.push([i + 9]);
-                }
-                else {
-                    possibleMoves.push([-1, i + 9]);
-                }
-            }
+            if (chessPieces[i + 9].id.match('^black.*'))
+                possibleHits.push(i + 9);
+        } else if ((i + 1) % 8 === 0) {
+            if (chessPieces[i + 7].id.match('^black.*'))
+                possibleHits.push(i + 7);
+        } else {
+            if (chessPieces[i + 7].id.match('^black.*'))
+                possibleHits.push(i + 7);
+            if (chessPieces[i + 9].id.match('^black.*'))
+                possibleHits.push(i + 9);
         }
     }
-
     highlight_possible_moves();
 }
 
-function king(el, i) {}
+function king(i) {
 
-function queen(el, i) {}
+}
 
-function bishop(el, i) {}
+function queen(i) {}
 
-function rook(el, i) {}
+function bishop(i) {
+    const edgeCase = (position) => (position % 8 === 0 || (position + 1) % 8 === 0);
+    for (let pos = i + 9; pos < 64; pos += 9) {
+        if (pieces.includes(chessPieces[pos].id))
+            possibleMoves.push(pos);
+        else {
+            if (!checkTurn(chessPieces[pos]))
+                possibleHits.push(pos);
+            break;
+        }
+        if (edgeCase(pos))
+            break;
+    }
+
+    for (let pos = i - 9; pos >= 0; pos -= 9) {
+        if (pieces.includes(chessPieces[pos].id))
+            possibleMoves.push(pos);
+        else {
+            if (!checkTurn(chessPieces[pos]))
+                possibleHits.push(pos);
+            break;
+        }
+        if (edgeCase(pos))
+            break;
+    }
+
+    for (let pos = i + 7; pos < 64; i += 7) {
+        if (pieces.includes(chessPieces[pos].id))
+            possibleMoves.push(pos);
+        else {
+            if (!checkTurn(chessPieces[pos]))
+                possibleHits.push(pos);
+            break;
+        }
+        if (edgeCase(pos))
+            break;
+    }
+
+    for (let pos = i - 7; pos >= 0; i -= 7) {
+        if (pieces.includes(chessPieces[pos].id))
+            possibleMoves.push(pos);
+        else {
+            if (!checkTurn(chessPieces[pos]))
+                possibleHits.push(pos);
+            break;
+        }
+        if (edgeCase(pos))
+            break;
+    }
+    highlight_possible_moves();
+}
+
+function rook(i) {
+    for (let pos = i + 8; pos < 64; pos += 8) {
+        if (pieces.includes(chessPieces[pos].id))
+            possibleMoves.push(pos);
+        else {
+            if (!checkTurn(chessPieces[pos]))
+                possibleHits.push(pos);
+            break;
+        }
+    }
+
+    for (let pos = i - 8; pos >= 0; pos -= 8) {
+        if (pieces.includes(chessPieces[pos].id))
+            possibleMoves.push(pos);
+        else {
+            if (!checkTurn(chessPieces[pos]))
+                possibleHits.push(pos);
+            break;
+        }
+    }
+
+    for (let pos = i + 1; pos < Math.ceil((i + 1) / 8) * 8; i++) {
+        if (pieces.includes(chessPieces[pos].id))
+            possibleMoves.push(pos);
+        else {
+            if (!checkTurn(chessPieces[pos]))
+                possibleHits.push(pos);
+            break;
+        }
+    }
+
+    for (let pos = i - 1; pos >= Math.floor(i / 8) * 8; i--) {
+        if (pieces.includes(chessPieces[pos].id))
+            possibleMoves.push(pos);
+        else {
+            if (!checkTurn(chessPieces[pos]))
+                possibleHits.push(pos);
+            break;
+        }
+    }
+    highlight_possible_moves();
+}
 
 
 // no obstruction
-function knight(el, i) {}
+function knight(i) {}
